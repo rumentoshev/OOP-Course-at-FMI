549c4bd4bbe7280618dca90b6f2910f1ba60c266
rumentoshev <monkey23toshev@abv.bg>
2024-06-05 00:59:48 +0300

    =

--- HW3/ExtrF.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "PF.h"
-
-class ExtrF : public PF
-{
-
-};
--- HW3/Pair.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <cstdint>
-struct Pair
-{
-	bool isDef = false; //Think about it!!!
-	int32_t result = 0;
-};
